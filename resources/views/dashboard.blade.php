0ccc6ac605ee11007c5e1419fe3f3ab8eb80af1e
i3asm <i3asmmail@gmail.com>
2020-01-28 08:27:50 +0300

    link the backend with the frontend

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -102,13 +102,27 @@
         <div class="row justify-content-center">
             <div class="col-md-9">
                 <div class="card">
+                    <div class="card-header rtl">محتويات الصفحة الرئيسية</div>
+                    <div class="card-body">
+                        <p> hello </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-9">
+                <div class="card">
                     <div class="card-header rtl">تعديل الحسابات
                         <div class="bg-warning">
                             <h2 class="text-danger rtl">
                                 خاصية تعديل الحسابات هي على مسؤليتك الشخصية.
                             </h2>
                             <h5 class="text-danger rtl">
-                                كل حساب تضيفه تصبح له كل الصلاحيات الممكنة في التعديل والحذف والإضافة، لا يمكن التراجع عن أي تغيير.
+                                كل حساب تضيفه تصبح له كل الصلاحيات الممكنة في التعديل والحذف والإضافة، لا يمكن التراجع
+                                عن أي تغيير.
                             </h5>
                         </div>
                     </div>
@@ -121,27 +135,5 @@
             </div>
         </div>
     </div>
-
-{{--    <div class="container">--}}
-{{--        <div class="row justify-content-center">--}}
-{{--            <div class="col-md-9">--}}
-{{--                <div class="card">--}}
-{{--                    <div class="card-header rtl">تعديل الحسابات--}}
-{{--                        <div class="bg-warning">--}}
-{{--                            <h2 class="text-danger rtl">--}}
-{{--                                خاصية تعديل الحسابات هي على مسؤليتك الشخصية.--}}
-{{--                            </h2>--}}
-{{--                            <h5 class="text-danger rtl">--}}
-{{--                                كل حساب تضيفه تصبح له كل الصلاحيات الممكنة في التعديل والحذف والإضافة--}}
-{{--                            </h5>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                    <div class="card-body">--}}
-{{--                        @component('dashboard.register') @endcomponent--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
 
 @endsection
